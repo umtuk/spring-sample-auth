29affe76fdbcfd74627d50d6fa473a019422500f
umtuk <taeukum@gmail.com>
2023-10-20 00:23:18 +0900

    Initial commit

---
+++ src/main/java/org/umtuk/sample/auth/AuthSampleApplication.java
@@ -0,0 +1,13 @@
+package org.umtuk.sample.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/umtuk/sample/auth/AuthSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.umtuk.sample.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
