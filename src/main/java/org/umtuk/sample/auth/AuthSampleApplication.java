4c3a66867df10c515f807e6606eb318d6f1cbded
umtuk <taeukum@gmail.com>
2023-10-23 16:26:04 +0900

    #1 feat(setup): minimal code and configuration for Mongo, Redis

--- src/main/java/org/umtuk/sample/auth/AuthSampleApplication.java
+++ src/main/java/org/umtuk/sample/auth/AuthSampleApplication.java
@@ -2,8 +2,12 @@ package org.umtuk.sample.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoAuditing
+@EnableMongoRepositories
 public class AuthSampleApplication {
 
 	public static void main(String[] args) {
